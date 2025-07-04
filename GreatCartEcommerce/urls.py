b02862293d03b6dfcdbd82a61765166d1d8ec105
ubaid <ubaidsaleem825@gmail.com>
2025-07-04 16:24:31 +0500

    create cart app

--- GreatCartEcommerce/urls.py
+++ GreatCartEcommerce/urls.py
@@ -24,5 +24,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.Home, name='home'),
     path('store/', include('store.urls')),
+    path('cart/', include('cart.urls')),
     
 ] +static (settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) 
